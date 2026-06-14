3b1762374802d0549333e703acbab9a1355f9494
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-14 18:44:16 -0500

    c00: add ex01 (ft_print_alphabet)

---
+++ piscine/c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Bit <BitSwapped@proton.me>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/14 18:32:30 by Bit               #+#    #+#             */
+/*   Updated: 2026/06/14 18:42:51 by Bit              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
