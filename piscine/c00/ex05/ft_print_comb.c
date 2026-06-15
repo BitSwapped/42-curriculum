7549aa5390b70ea3fd86fd3c63dcc0d8db899364
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-14 19:44:10 -0500

    c00: add ex05 (ft_print_comb)

---
+++ piscine/c00/ex05/ft_print_comb.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Bit <BitSwapped@proton.me>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/14 19:06:43 by Bit               #+#    #+#             */
+/*   Updated: 2026/06/14 19:30:59 by Bit              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_putchar(i + '0');
+				ft_putchar(j + '0');
+				ft_putchar(k + '0');
+				ft_putchar(',');
+				ft_putchar(' ');
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+}
