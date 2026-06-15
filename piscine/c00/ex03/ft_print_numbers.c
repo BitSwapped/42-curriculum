2d998dcd196fe59ae412191c469ddbf75b5f2181
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-14 19:43:37 -0500

    c00: add ex03 (ft_print_numbers)

---
+++ piscine/c00/ex03/ft_print_numbers.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Bit <BitSwapped@proton.me>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/14 18:53:29 by Bit               #+#    #+#             */
+/*   Updated: 2026/06/14 19:00:29 by Bit              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	int	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
